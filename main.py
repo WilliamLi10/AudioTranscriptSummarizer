3dea7398adc56094f706ee13186240d388c7faf3
Jia Huang <jiahuang2468@gmail.com>
2022-09-24 15:37:38 -0400

    Now outputs to a pdf

--- main.py
+++ main.py
@@ -1,22 +1,22 @@
-
+import sys
 from condensor import *
 from pdf_converter import *
+from create_transcript import *
+
+CHARACTER_LIMIT = 100
 
 def main():
-    file = open("book-1984.txt", "r")
+
+    wav_file = str(sys.argv[1])
+    create_transcript(wav_file)
+    
+    file = open("transcript.txt", "r")
     text = file.read()
     document = nlp(text)
     file.close()
 
-    summary = generate_summary(3, document)
-    
-    new_file = open("text.txt", "w")
-    for line in summary:
-        new_file.write(str(line))
-    new_file.close()
-    
-
-    pdf_converter("text.txt")
+    summary = str(generate_summary(3, document)).split(" ")
+    pdf_converter(summary)
     
 
 if __name__ == "__main__":
--- pdf_converter.py
+++ pdf_converter.py
@@ -1,8 +1,10 @@
 from fpdf import FPDF
+
+CHARACTER_LIMIT = 100
   
 # save FPDF() class into
 # a variable pdf
-def pdf_converter(text_file):
+def pdf_converter(summary):
     pdf = FPDF()  
     
     # Add a page
@@ -13,11 +15,21 @@ def pdf_converter(text_file):
     pdf.set_font("Times", size = 12)
     
     # open the text file in read mode
-    f = open(text_file, "r")
-    
+
     # insert the texts in pdf
-    for x in f:
-        pdf.cell(200, 10, txt = x, ln = 2, align = 'L')
+
+    char_count = 0
+    line = ""
+    for word in summary:
+        char_count += len(word) + 1
+        if char_count < CHARACTER_LIMIT:
+            line += word + " "
+        else:
+            char_count = len(word) + 1
+            pdf.cell(200, 20, txt = line, ln = 2, align = 'L')
+            line = word + " "
+
+
     
     # save the pdf with name .pdf
     pdf.output("converted.pdf")  
